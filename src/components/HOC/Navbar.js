59fd82d3d87da783443f69aa500e62c865c3899e
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2023-02-08 16:02:06 +0530

    Navlink CSS change

--- src/components/HOC/Navbar.js
+++ src/components/HOC/Navbar.js
@@ -14,147 +14,162 @@ import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import NITHLogo from "../../assests/nith-logo.png";
 
 import {
-  verifiedPages,
-  verifiedSettings,
-  unverifiedSettings,
+    verifiedPages,
+    verifiedSettings,
+    unverifiedSettings,
 } from "../constants/navbarConstants";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/actionThunk/authThunk";
 
+const NAVLINK_STYLE = {
+    textDecoration: "none",
+    color: "#fff",
+    fontFamily: "monospace",
+    fontWeight: 700,
+    letterSpacing: ".05rem",
+    fontSize: "16px",
+    padding: "10px",
+    boxShadow: "20px 20px 50px 10px #164b9f inset",
+    borderRadius: "10px",
+    margin: "auto 15px",
+};
+
 function Navbar() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const accessToken = useSelector((state) => state.auth.accessToken);
+    const accessToken = useSelector((state) => state.auth.accessToken);
 
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  return (
-    <>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: {
-                  md: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                },
-              }}
-            >
-              <NavLink
-                to="/"
-                style={{
-                  textDecoration: "none",
-                  color: "#fff",
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".05rem",
-                  fontSize: "24px",
-                }}
-              >
-                TimeTable Generator
-              </NavLink>
-              <Box
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  flexGrow: 1,
-                  justifyContent: "center",
-                }}
-              >
-                {verifiedPages.map((page) => (
-                  <NavLink
-                    className={({ isActive }) =>
-                      isActive ? "activeRoute" : ""
-                    }
-                    to={page.href}
-                    key={page.href}
-                    style={{
-                      textDecoration: "none",
-                      color: "#fff",
-                      fontFamily: "monospace",
-                      fontWeight: 700,
-                      letterSpacing: ".05rem",
-                      fontSize: "16px",
-                      padding: "10px",
-                      boxShadow: "20px 20px 50px 10px #164b9f inset",
-                      borderRadius: "10px",
-                      margin: "auto 15px",
-                    }}
-                  >
-                    {page.name}
-                  </NavLink>
-                ))}
-              </Box>
-            </Box>
+    return (
+        <>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {
+                                    md: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                },
+                            }}
+                        >
+                            <NavLink
+                                to="/"
+                                style={{
+                                    textDecoration: "none",
+                                    color: "#fff",
+                                    fontFamily: "monospace",
+                                    fontWeight: 700,
+                                    letterSpacing: ".05rem",
+                                    fontSize: "24px",
+                                }}
+                            >
+                                TimeTable Generator
+                            </NavLink>
+                            <Box
+                                sx={{
+                                    display: { xs: "none", md: "flex" },
+                                    flexGrow: 1,
+                                    justifyContent: "center",
+                                }}
+                            >
+                                {verifiedPages.map((page) => (
+                                    <NavLink
+                                        to={page.href}
+                                        key={page.href}
+                                        style={({ isActive }) =>
+                                            isActive
+                                                ? {
+                                                      ...NAVLINK_STYLE,
+                                                      color: "#1b2184",
+                                                      boxShadow:
+                                                          "rgb(176, 207, 255) 20px 20px 50px 10px inset",
+                                                  }
+                                                : NAVLINK_STYLE
+                                        }
+                                    >
+                                        {page.name}
+                                    </NavLink>
+                                ))}
+                            </Box>
+                        </Box>
 
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="NITH Logo" src={NITHLogo} />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {(accessToken ? verifiedSettings : unverifiedSettings).map(
-                  (setting) => (
-                    <MenuItem
-                      sx={{ minHeight: "auto" }}
-                      key={setting.name}
-                      onClick={() => {
-                        handleCloseUserMenu();
-                        setting.href === "logout"
-                          ? dispatch(logout())
-                          : navigate(`/${setting.href}`);
-                      }}
-                    >
-                      <Typography
-                        sx={{
-                          textAlign: "center",
-                          textDecoration: "none",
-                          color: "#000",
-                          fontFamily: "monospace",
-                          fontSize: { xs: "12px", md: "16px" },
-                        }}
-                      >
-                        {setting.name}
-                      </Typography>
-                    </MenuItem>
-                  )
-                )}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton
+                                    onClick={handleOpenUserMenu}
+                                    sx={{ p: 0 }}
+                                >
+                                    <Avatar alt="NITH Logo" src={NITHLogo} />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: "45px" }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {(accessToken
+                                    ? verifiedSettings
+                                    : unverifiedSettings
+                                ).map((setting) => (
+                                    <MenuItem
+                                        sx={{ minHeight: "auto" }}
+                                        key={setting.name}
+                                        onClick={() => {
+                                            handleCloseUserMenu();
+                                            setting.href === "logout"
+                                                ? dispatch(logout())
+                                                : navigate(`/${setting.href}`);
+                                        }}
+                                    >
+                                        <Typography
+                                            sx={{
+                                                textAlign: "center",
+                                                textDecoration: "none",
+                                                color: "#000",
+                                                fontFamily: "monospace",
+                                                fontSize: {
+                                                    xs: "12px",
+                                                    md: "16px",
+                                                },
+                                            }}
+                                        >
+                                            {setting.name}
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
 }
 export default Navbar;
