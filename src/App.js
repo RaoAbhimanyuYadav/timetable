104a5c995c941ed6b3305887a939edc681d0c1cd
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2023-05-09 15:02:46 +0530

    NavBar CSS completed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import store from "./components/redux/store";
 import { Provider } from "react-redux";
 import "./App.css";
 import RouteSelector from "./RouteSelector";
+import { amber, deepOrange, grey } from "@mui/material/colors";
+
+// 817e6a -- normal text
+// 0c1237 -- background
 
 const darkTheme = createTheme({
-  palette: {
-    mode: "light",
-  },
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#2196f3",
+        },
+        secondary: {
+            main: "#9c27b0",
+        },
+        divider: deepOrange[700],
+        background: {
+            default: "#0c1237",
+            paper: "#22135e",
+        },
+        text: {
+            primary: "#c2bb92",
+            secondary: "#ffa03a",
+        },
+    },
 });
 
 function App() {
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <RouteSelector />
-      </ThemeProvider>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <RouteSelector />
+            </ThemeProvider>
+        </Provider>
+    );
 }
 
 export default App;
